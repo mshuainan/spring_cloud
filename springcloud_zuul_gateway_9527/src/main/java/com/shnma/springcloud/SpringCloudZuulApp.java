7ed140ff7d0a2b87514456445141ce16cfe29b29
shnma <shnma@mail.nfsq.com.cn>
2019-10-06 16:55:27 +0800

    zuul网关相关

---
+++ springcloud_zuul_gateway_9527/src/main/java/com/shnma/springcloud/SpringCloudZuulApp.java
@@ -0,0 +1,14 @@
+package com.shnma.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudZuulApp {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringCloudZuulApp.class, args);
+	}
+}
